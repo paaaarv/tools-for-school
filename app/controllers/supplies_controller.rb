d1aec2fe49c38dbe823b73172d99c92fba23389f
Parv Kumar <parvathikumar92@gmail.com>
2018-04-10 20:51:51 +0000

    added basic form for new supply

--- app/controllers/supplies_controller.rb
+++ app/controllers/supplies_controller.rb
@@ -1,11 +1,27 @@
 require 'pry'
 class SuppliesController < ApplicationController
 
+
+  def new
+    @supply = Supply.new
+  end
+
+  def create
+    @supply = Supply.create(supply_params)
+    redirect_to '/'
+  end
+
   def donate
     @supply = Supply.find(params[:id])
     donations = params[:supply][:donations].to_f
     @supply.donations += donations
     @supply.save
     redirect_to '/'
+  end
+
+  private
+
+  def supply_params
+    params.require(:supply).permit(:name, :price, :quantity, :description, :category_id)
   end
 end
