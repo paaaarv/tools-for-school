6632494c6ee43b7111038559301e7cd1e0e6a7cf
Parv Kumar <parvathikumar92@gmail.com>
2018-04-10 19:11:45 +0000

    added seeds, fixed home view

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,6 @@
 @c2 = Category.find_or_create_by(name: "Furniture")
 @c3 = Category.find_or_create_by(name: "Miscellaneous")
 
-@s1 = Supply.find_or_create_by(name: "Tom Sawyer", price: "7.99", quantity: 30, description: "Need 30 books for 7th grade class.")
-@s2 = Supply.find_or_create_by(name: "Bookshelves", price: "70", quantity: 3, description: "Need new bookshelves for classroom.")
-@s3 = Supply.find_or_create_by(name: "Binders", price: "4.99", quantity: 11, description: "Need binders to organize classroom")
+@s1 = Supply.find_or_create_by(name: "Tom Sawyer", price: "7.99", quantity: 30, description: "Need 30 books for 7th grade class.", category_id: 1)
+@s2 = Supply.find_or_create_by(name: "Bookshelves", price: "70", quantity: 3, description: "Need new bookshelves for classroom.", category_id: 2)
+@s3 = Supply.find_or_create_by(name: "Binders", price: "4.99", quantity: 11, description: "Need binders to organize classroom", category_id: 3)
