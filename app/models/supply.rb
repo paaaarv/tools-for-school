297a4e327eed0d940663824f8cbbcb683b49d9b4
Parv Kumar <parvathikumar92@gmail.com>
2018-04-10 19:15:29 +0000

    added amount to goal method

--- app/models/supply.rb
+++ app/models/supply.rb
@@ -1,4 +1,11 @@
 class Supply < ActiveRecord::Base
   belongs_to :user
-  belongs_to :category 
+  belongs_to :category
+
+
+
+  def amount_to_goal
+    goal = self.price * self.quantity
+    "$#{self.donations} out of $#{goal} raised."
+  end
 end
