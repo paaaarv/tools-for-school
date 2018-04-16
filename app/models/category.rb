0784c99f8ce535b6e2909ae15b457d12d2956ab3
Parv Kumar <parvathikumar92@gmail.com>
2018-04-16 16:59:35 +0000

    added model validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ActiveRecord::Base
   has_many :supplies
   has_many :users, through: :supplies
+
+  validates :name, presence: true 
 end
--- app/models/supply.rb
+++ app/models/supply.rb
@@ -2,6 +2,11 @@ class Supply < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
 
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :quantity, presence: true
+  validates :category_id, presence: true 
+
 
 
 end
