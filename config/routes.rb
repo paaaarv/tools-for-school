0afdb3c16a9d3e2cc71606ed133abd888164c0fe
paaaarv <pkumar@trinity.edu>
2018-09-10 15:24:52 -0500

    successfully got the see more button to work in order to append a show page

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,6 @@ Rails.application.routes.draw do
   end
   get '/users/my_profile' => 'users#show'
   get '/categories/:id/supplies_data' => "categories#supplies_data"
-  get '/categories/:category_id/supplies/:id/data'=> "supplies#data"
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", :registrations => "users/registrations"}
 
 
