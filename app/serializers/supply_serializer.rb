117fe155c27f5eb9f2ca8dc39fefc2dcf3036284
paaaarv <pkumar@trinity.edu>
2018-09-11 16:05:17 -0500

    started adding supply information via jquery ajax

--- app/serializers/supply_serializer.rb
+++ app/serializers/supply_serializer.rb
@@ -1,4 +1,4 @@
 class SupplySerializer < ActiveModel::Serializer
-  attributes :id, :name, :price, :category_id
+  attributes :id, :name, :price, :category_id, :quantity, :description
   belongs_to :category
 end
