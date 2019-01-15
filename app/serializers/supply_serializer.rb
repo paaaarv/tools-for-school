faaeaebaa73e19016512a6abc7956ca07c0fe00b
paaaarv <pkumar@trinity.edu>
2019-01-15 16:13:47 -0600

    added list of supplies to show up on first link click

--- app/serializers/supply_serializer.rb
+++ app/serializers/supply_serializer.rb
@@ -1,4 +1,4 @@
 class SupplySerializer < ActiveModel::Serializer
-  attributes :id, :name, :price, :category_id, :quantity, :description, :user_id
+  attributes :id, :name, :price, :category_id, :quantity, :description, :user_id, :donations
   belongs_to :category
 end
